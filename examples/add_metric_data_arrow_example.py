cd83a486e65b9925a791769efd027299368fb0af
Michal Chrobok <michal.chrobok@hitachivantara.com>
2025-07-01 12:19:59 +0200

    Small changes in example scritps

--- examples/add_metric_data_arrow_example.py
+++ examples/add_metric_data_arrow_example.py
@@ -7,10 +7,10 @@ import os
 
 def main() -> None:
     cvec = CVec(
-        host=os.environ.get("CVEC_HOST", "https://sandbox.cvector.dev"),
-        email=os.environ.get("CVEC_EMAIL", "your-email@example.com"),
+        host=os.environ.get("CVEC_HOST", "https://your-subdomain.cvector.dev"),
+        email=os.environ.get("CVEC_EMAIL", "your-email@cvector.app"),
         password=os.environ.get("CVEC_PASSWORD", "your-password"),
-        publishable_key=os.environ.get("CVEC_PUBLISHABLE_KEY", "your-supabase-publishable-key"),
+        publishable_key=os.environ.get("CVEC_PUBLISHABLE_KEY", "your-cvec-publishable-key"),
     )
     test_metric_name = "python-sdk/test"
     print("\nAdding new metric data using Arrow...")
