03a295c598887d11b84328dee01edb3a845e9c2a
Amy Ni <ani@cvector.energy>
2025-09-03 14:36:27 -0400

    chore: remove unused code

--- src/cvec/models/__init__.py
+++ src/cvec/models/__init__.py
@@ -1,26 +1,8 @@
 from .metric import Metric, MetricDataPoint
-from .modeling import (
-    FetchModelingReadingsRequest,
-    LatestReadingsRequest,
-    LatestReadingsResponse,
-    LatestReadingsResponseItem,
-    ModelingReadingsDataResponse,
-    ModelingReadingsGroup,
-    ModelingReadingModel,
-    TagSourceType,
-)
 from .span import Span
 
 __all__ = [
     "Metric",
     "MetricDataPoint",
     "Span",
-    "ModelingReadingsDataResponse",
-    "ModelingReadingsGroup",
-    "ModelingReadingModel",
-    "FetchModelingReadingsRequest",
-    "LatestReadingsRequest",
-    "LatestReadingsResponse",
-    "LatestReadingsResponseItem",
-    "TagSourceType",
 ]
